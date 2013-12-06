5c3304695445368d493e96ab3397885f110336cd
grandbora <bora@soundcloud.com>
2013-12-06 20:32:30 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Portal2sounds::Application.config.secret_key_base = 'b4e89e4cc75472472a42e53e13864c2ab4d632bdf59cfcf2eeccc4f600875990c478d0400f3b8a6c43f17c07bc896f3d5f6b9c9d292e9343aa04f7d7acab707d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Portal2sounds::Application.config.session_store :cookie_store, key: '_portal2sounds_session'
